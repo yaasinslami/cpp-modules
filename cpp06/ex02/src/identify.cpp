1efb383541f3433e6186a82494231e39cd195633
yaasinslami <yassinx4002@gmail.com>
2025-09-05 23:45:54 +0100

    Update cpp06

--- cpp06/ex02/src/identify.cpp
+++ cpp06/ex02/src/identify.cpp
@@ -2,8 +2,34 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <locale>
 
 Base * generate(void)
 {
+	int	r = std::rand() % 3;
 
+	if (r == 0)
+	{
+		std::cout << "A generated" << std::endl;
+		return new A();
+	}
+	else if (r == 1)
+	{
+		std::cout << "B generated" << std::endl;
+		return new B();
+	}
+	else
+	{
+		std::cout << "C generated" << std::endl;
+		return new C();
+	}
 }
+
+void	identify(Base* p)
+{
+	try
+	{
+		
+	}
+}
+
